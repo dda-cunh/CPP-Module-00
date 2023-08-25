0f55506ae3d4df4b41ff72474f0f204470dfde6b
dda-cunh <dda-cunh@student.42lisboa.com>
2023-08-25 18:17:50 +0100

    Changed to be less C-like

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,24 +1,17 @@
 #include <iostream>
 
-void    printCap(char c)
+int	main(int ac, char **av)
 {
-    if (c >= 'a' && c <= 'z')
-        c -= 32;
-    std::cout << c;
-}
-
-int main(int ac, char **av)
-{
-    if (ac == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    else
-    {
-        for (int i = 1; i < ac; i++)
-        {
-            std::string s = av[i];
-            for (unsigned long j = 0; j < s.size(); j++)
-                printCap(s[j]);
-        }
-    }
-    std::cout << std::endl;
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			std::string s = av[i];
+			for (unsigned long j = 0; j < s.size(); j++)
+				std::cout << ((char)toupper(s[j]));
+		}
+	}
+	std::cout << std::endl;
 }
